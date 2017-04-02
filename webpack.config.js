60d10bb05425a26dafe7cd3c7f3f4be840240ea3
Tarashish Mishra <sunu0000@gmail.com>
2017-04-02 12:50:03 +0530

    use the es2015 babel preset while building

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,14 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015',]
+        }
+      },
     ]
   }
 }
