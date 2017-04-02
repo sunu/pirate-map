5500bb3cb8d0a830ba2f7b01f28d22d6d6bdd791
Tarashish Mishra <sunu0000@gmail.com>
2017-04-02 20:30:17 +0530

    Draw path to treasure

--- index.js
+++ index.js
@@ -31,38 +31,38 @@ class PirateMap {
     ctx.fill()
 
     // lower niddle
-    ctx.beginPath();
-    ctx.moveTo(compassWidth/2, this.canvas.height);
-    ctx.lineTo(compassWidth/2 - 5, this.canvas.height - compassHeight/2);
-    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight/2);
-    ctx.lineTo(compassWidth/2, this.canvas.height);
-    ctx.fillStyle = "#DAE1EF";
-    ctx.fill();
+    ctx.beginPath()
+    ctx.moveTo(compassWidth/2, this.canvas.height)
+    ctx.lineTo(compassWidth/2 - 5, this.canvas.height - compassHeight/2)
+    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight/2)
+    ctx.lineTo(compassWidth/2, this.canvas.height)
+    ctx.fillStyle = "#DAE1EF"
+    ctx.fill()
 
-    ctx.beginPath();
-    ctx.moveTo(compassWidth/2, this.canvas.height);
-    ctx.lineTo(compassWidth/2 + 5, this.canvas.height - compassHeight/2);
-    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight/2);
-    ctx.lineTo(compassWidth/2, this.canvas.height);
-    ctx.fillStyle = "#91AEDC";
-    ctx.fill();
+    ctx.beginPath()
+    ctx.moveTo(compassWidth/2, this.canvas.height)
+    ctx.lineTo(compassWidth/2 + 5, this.canvas.height - compassHeight/2)
+    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight/2)
+    ctx.lineTo(compassWidth/2, this.canvas.height)
+    ctx.fillStyle = "#91AEDC"
+    ctx.fill()
 
     // upper niddle
-    ctx.beginPath();
-    ctx.moveTo(compassWidth/2, this.canvas.height - compassHeight);
-    ctx.lineTo(compassWidth/2 - 5, this.canvas.height - compassHeight/2);
-    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight/2);
-    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight);
-    ctx.fillStyle = "#FF532B";
-    ctx.fill();
+    ctx.beginPath()
+    ctx.moveTo(compassWidth/2, this.canvas.height - compassHeight)
+    ctx.lineTo(compassWidth/2 - 5, this.canvas.height - compassHeight/2)
+    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight/2)
+    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight)
+    ctx.fillStyle = "#FF532B"
+    ctx.fill()
 
-    ctx.beginPath();
-    ctx.moveTo(compassWidth/2, this.canvas.height - compassHeight);
-    ctx.lineTo(compassWidth/2 + 5, this.canvas.height - compassHeight/2);
-    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight/2);
-    ctx.lineTo(compassWidth/2, this.canvas.height -compassHeight);
-    ctx.fillStyle = "#DC052C";
-    ctx.fill();
+    ctx.beginPath()
+    ctx.moveTo(compassWidth/2, this.canvas.height - compassHeight)
+    ctx.lineTo(compassWidth/2 + 5, this.canvas.height - compassHeight/2)
+    ctx.lineTo(compassWidth/2, this.canvas.height - compassHeight/2)
+    ctx.lineTo(compassWidth/2, this.canvas.height -compassHeight)
+    ctx.fillStyle = "#DC052C"
+    ctx.fill()
   }
 
   /* Generate alpha value based on co-ordinate of pixel
@@ -93,11 +93,60 @@ class PirateMap {
     return value
   }
 
+  /* Generate a random integer between min and max */
+  getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  /* Guide our pirates to the treasure */
+  drawPath () {
+    let totalPoints = this.lands.length
+    let sectionLength = Math.floor(totalPoints / 5)
+    let treasure = this.lands[
+      this.getRandomInt(totalPoints - sectionLength, totalPoints)
+    ]
+    let ctx = this.canvas.getContext('2d')
+    ctx.beginPath()
+    ctx.strokeStyle = "#FF532B"
+    ctx.lineWidth = 5
+    ctx.moveTo(treasure[0] - 10, treasure[1] - 10)
+    ctx.lineTo(treasure[0] + 10, treasure[1] + 10)
+    ctx.stroke()
+    ctx.moveTo(treasure[0] + 10, treasure[1] - 10)
+    ctx.lineTo(treasure[0] - 10, treasure[1] + 10)
+    ctx.stroke()
+
+    let path = []
+    let start = 0
+    while (start < totalPoints - sectionLength) {
+      let point = this.lands[this.getRandomInt(start, start + sectionLength)]
+      path.push(point)
+      start = start + sectionLength
+    }
+    path.push(treasure)
+
+    ctx.setLineDash([10, 10])
+    ctx.beginPath()
+    // draw a smooth curve through all the points in path
+    // from http://stackoverflow.com/a/7058606
+    // move to the first point
+    ctx.moveTo(path[0][0], path[0][1])
+    for (var i = 1; i < path.length - 2; i++) {
+      var xc = (path[i][0] + path[i + 1][0]) / 2
+      var yc = (path[i][1] + path[i + 1][1]) / 2
+      ctx.quadraticCurveTo(path[i][0], path[i][1], xc, yc)
+    }
+     // curve through the last two path
+     ctx.quadraticCurveTo(path[i][0], path[i][1], path[i+1][0] ,path[i+1][1])
+     ctx.stroke()
+  }
+
   /* Get the wheels running. Make shit happen. */
   drawMap () {
     let ctx = this.canvas.getContext('2d')
     var image = ctx.createImageData(this.canvas.width, this.canvas.height)
     var data = image.data
+    this.lands = []
     for (var x = 0; x < this.canvas.width; x++) {
       for (var y = 0; y < this.canvas.height; y++) {
         var value = this.generateNoise(x, y)
@@ -109,23 +158,25 @@ class PirateMap {
           data[cell + 1] = 140
           data[cell + 2] = 120
           data[cell + 3] = value // alpha.
+          this.lands.push([x, y])
         } else if (value > 70) {
           // shallow waters
           data[cell] = 0
-          data[cell + 1] = 153
-          data[cell + 2] = 255
+          data[cell + 1] = 113
+          data[cell + 2] = 185
           data[cell + 3] = (255 - value) / 1.05 // alpha.
         } else {
           // deep waters
           data[cell] = 0
-          data[cell + 1] = 153
-          data[cell + 2] = 255
+          data[cell + 1] = 113
+          data[cell + 2] = 185
           data[cell + 3] = 255 - value // alpha.
         }
       }
     }
     ctx.putImageData(image, 0, 0)
     this.drawCompass()
+    this.drawPath()
   }
 }
 
