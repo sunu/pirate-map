12bd6a24af4735410c71803e6344d5370c0b5760
Tarashish Mishra <sunu0000@gmail.com>
2017-04-02 14:19:35 +0530

    Frequncies and octaves applied. Different colors for shallow and deep water

--- index.js
+++ index.js
@@ -4,34 +4,54 @@ var simplex = new SimplexNoise(),
   value2d = simplex.noise2D(5, 10)
 
 var canvas = document.getElementsByTagName('canvas')[0]
-canvas.width = 500
-canvas.height = 500
+canvas.width = 1000
+canvas.height = 600
 var ctx = canvas.getContext('2d')
 var image = ctx.createImageData(canvas.width, canvas.height)
 var data = image.data
 
+let generateNoise = (nx, ny, frequency) => {
+  // mix things up with varied frquencies and octaves
+  let value = (
+    1* simplex.noise2D(frequency * nx, frequency * ny) +
+    0.5 * simplex.noise2D(2 * frequency * nx, 2 * frequency * ny) +
+    0.25 * simplex.noise2D(4 * frequency * nx, 4 * frequency * ny) +
+    0.125 * simplex.noise2D(8 * frequency * nx, 8 * frequency * ny)
+  )
+  // convert from -1 to 1 range to 0 to 1 range
+  // https://stackoverflow.com/q/929103/
+  value = (value + 1) / 2
+  value *= 255
+  return value
+}
+
 for (var x = 0; x < canvas.width; x++) {
   for (var y = 0; y < canvas.height; y++) {
-    // convert from -1 to 1 range to 0 to 1 range
-    // https://stackoverflow.com/q/929103/
-    var value = (simplex.noise2D(x / 100, y / 100) + 1) / 2
-    value *= 255
+    let nx = x/canvas.width - 0.5
+    let ny = y/canvas.height - 0.5
+    let frequency = 1.5
+    var value = generateNoise(nx, ny, frequency)
     var cell = (x + y * canvas.width) * 4
 
-    if (value < 80) {
+    if (value < 120) {
       // land
-      data[cell] = 158
-      data[cell + 1] = 158
-      data[cell + 2] = 158
-      data[cell + 3] = (255 - value) // alpha.
-    } else {
-      // water
+      data[cell] = 160
+      data[cell + 1] = 140
+      data[cell + 2] = 120
+      data[cell + 3] = 255 - value // alpha.
+    } else if (value < 150) {
+      // shallow waters
       data[cell] = 0
       data[cell + 1] = 153
       data[cell + 2] = 255
-      data[cell + 3] = value // alpha.
+      data[cell + 3] = (value + 255) / 2.5 // alpha.
+    } else {
+      // deep waters
+      data[cell] = 0
+      data[cell + 1] = 153
+      data[cell + 2] = 255
+      data[cell + 3] = (value + 255) / 2.3// alpha.
     }
-    
   }
 }
 
